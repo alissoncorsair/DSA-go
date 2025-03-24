b8f5e4d5c01ebc89c9ffc481bb2e40f86e68ec45
alissoncorsair <alissoncorsair@hotmail.com>
2025-03-24 20:37:22 -0300

    improving .27 readability

--- merge-sort/merge-sort.go
+++ merge-sort/merge-sort.go
@@ -14,37 +14,35 @@ func mergeSort(arr []int) []int {
 	if len(arr) < 2 {
 		return arr
 	}
-	arrA := arr[:len(arr)/2]
-	arrB := arr[len(arr)/2:]
-	arrA = mergeSort(arrA)
-	arrB = mergeSort(arrB)
+	middle := len(arr) / 2
+	arrA := mergeSort(arr[:middle])
+	arrB := mergeSort(arr[middle:])
 
 	return merge(arrA, arrB)
 }
 
-func merge(arrA, arrB []int) []int {
-	c := make([]int, 0)
+func merge(leftArr, rightArr []int) []int {
+	sorted := make([]int, 0)
 
-	for len(arrA) > 0 && len(arrB) > 0 {
-
-		if arrA[0] > arrB[0] {
-			c = append(c, arrB[0])
-			arrB = arrB[1:]
+	for len(leftArr) > 0 && len(rightArr) > 0 {
+		if leftArr[0] > rightArr[0] {
+			sorted = append(sorted, rightArr[0])
+			rightArr = rightArr[1:]
 		} else {
-			c = append(c, arrA[0])
-			arrA = arrA[1:]
+			sorted = append(sorted, leftArr[0])
+			leftArr = leftArr[1:]
 		}
 	}
 
-	for len(arrA) > 0 {
-		c = append(c, arrA[0])
-		arrA = arrA[1:]
+	for len(leftArr) > 0 {
+		sorted = append(sorted, leftArr[0])
+		leftArr = leftArr[1:]
 	}
 
-	for len(arrB) > 0 {
-		c = append(c, arrB[0])
-		arrB = arrB[1:]
+	for len(rightArr) > 0 {
+		sorted = append(sorted, rightArr[0])
+		rightArr = rightArr[1:]
 	}
 
-	return c
+	return sorted
 }
